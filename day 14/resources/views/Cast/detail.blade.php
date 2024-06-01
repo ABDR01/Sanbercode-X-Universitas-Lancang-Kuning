2678dc3bc537bf608597a5211d6082bc2feffa28
ABDR01 <raufmoba123@gmail.com>
2024-06-01 23:19:09 +0700

    Day 14

--- day 14/resources/views/Cast/detail.blade.php
+++ day 14/resources/views/Cast/detail.blade.php
@@ -10,7 +10,7 @@
 <h6>{{$Cast->name}}</h6>
 <h6>{{$Cast->umur}}</h6>
 <p>{{$Cast->bio}}</p>
-
+<a href='/Cast'>Kembali</a>
 
 
 @endsection
--- day 14/resources/views/welcome.blade.php
+++ day 14/resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
     <li><a href='/register'>ke Biodata</a></li>
     <li><a href='/'>Home</a></li>
     <li><a href='/'>Sama aja</a></li>
+    <li><a href='/Cast'>Ke halaman Cast</a></li>
 </ul>
 
 @endsection
